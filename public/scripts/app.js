dede2e2f069ba5004e9b91db1d2be187cec919a3
Jason Vongsana <jvongsana@hotmail.com>
2020-10-18 22:03:51 +0000

    CSS update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,32 +1,53 @@
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+
+  return div.innerHTML;
+};
+const createTodoInstance = function(todo) {
+  const title = escape(todo);
+  const $instance = `
+  <div class="to-do-instance">
+    <p>${title}</p>
+    <span>
+      <button class="btn btn-outline-secondary">Edit</button>
+      <button class="btn btn-outline-danger">Delete</button>
+    </span>
+  </div>`;
+
+  return $instance;
+};
+
 $(() => {
+  //Ajax post request of todos data
+  $('.filter button').on('click', function(event) {
+    event.preventDefault();
 
+    $.ajax({
+      method: "GET",
+      url: "/api/categories"
+    }).done((categories) => {
+      for (const todo of categories) {
+        const todoInstance = createTodoInstance(todo);
 
+        $(".todos_container").append(todoInstance);
+        // $("<div class=\"to-do-instance\">").text(todo).appendTo($(".todos_container"));
+      }
+    });
 
-//Ajax post request of todos data
-$('.filter button').on('click', function(event) {
-  event.preventDefault();
-
-  $.ajax({
-    method: "GET",
-    url: "/api/categories"
-  }).done((categories) => {
-    for(todo of categories) {
-      $("<div>").text(todo).appendTo($(".todos_container"));
-    }
+    // $.ajax({
+    //   method: "GET",
+    //   url: "/api/users"
+    // }).done((users) => {
+    //   for(user in users) {
+    //     $("<div>").text(user.name).appendTo($(".todos_container"));
+    //   }
+    // });
   });
 
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user in users) {
-  //     $("<div>").text(user.name).appendTo($(".todos_container"));
-  //   }
-  // });
-
-
-})
-
-
+  $('.to-do-instance').on('click', function(event) {
+    event.preventDefault();
+    alert('test');
+  });
 
 });
