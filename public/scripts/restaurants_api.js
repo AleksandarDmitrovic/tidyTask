83d9f8331332f1fcfa35c35d35d030b3fe9394f9
Jason Vongsana <jvongsana@hotmail.com>
2020-10-18 07:16:51 +0000

    Restaurant API check

---
+++ public/scripts/restaurants_api.js
@@ -0,0 +1,16 @@
+const zomato = require('zomato-api');
+const client = zomato({
+  userKey: process.env.ZOMATO_API_KEY
+});
+
+const checkRestoApi = function(search) {
+  return client.search({q: `${search}`})
+    .then(function(body) {
+      console.log(body.results_found);
+    })
+    .catch(function(err) {
+      console.log('Error: ', err.statusCode);
+    });
+};
+
+module.exports = { checkRestoApi };
