59edfb5dd99e284138ed87391012e9ca3f7bb16d
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-10-20 20:46:17 +0000

    Temp routes for fake demo logins

--- routes/login.js
+++ routes/login.js
@@ -4,12 +4,22 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("login");
+    if (req.session.user_id) {
+      res.redirect('/');
+    } else {
+      res.render("login");
+    }
   });
 
   router.get('/:id', (req, res) => {
     req.session.user_id = req.params.id;
+    console.log('req.params :', req.params);
     res.redirect('/');
   });
+
+  router.post("/login", (req, res) => {
+    res.redirect('/1');
+  });
+
   return router;
 };
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
+const cookieSession = require("cookie-session");
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -30,6 +31,10 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 app.use(express.static("public"));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['super-long-secret-keys', 'typically-not-embedded-in-code']
+}));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -53,7 +58,11 @@ app.use("/login", loginRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  if (req.session.user_id) {
+    res.render("index");
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.listen(PORT, () => {
