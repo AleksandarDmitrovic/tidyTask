ebebbc0c42060ad6e5b0264284edb86a3fd5c35a
Jason Vongsana <jvongsana@hotmail.com>
2020-10-21 16:53:21 +0000

    edit profile field requirements

--- routes/edit-profile.js
+++ routes/edit-profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const e = require('express');
 
 module.exports = (db) => {
 
@@ -8,14 +9,48 @@ module.exports = (db) => {
     const  userId  = req.session.user_id;
     const { name, email, password } = req.query;
     const hashedPassword = bcrypt.hashSync(password, 10);
-    const queryParams = [userId, name, email, hashedPassword];
+    let queryString = ``;
+    const queryParams = [];
 
-    const queryString = `
-    UPDATE users
-    SET name = $2,
-    email = $3,
-    password = $4
-    WHERE id = $1`;
+    if (name === '') {
+      queryParams.push(userId);
+      queryParams.push(email);
+      queryParams.push(hashedPassword);
+      queryString += `
+      UPDATE users
+      SET email = $2,
+      password = $3
+      WHERE id = $1`;
+    } else if (email === '') {
+      queryParams.push(userId);
+      queryParams.push(name);
+      queryParams.push(hashedPassword);
+      queryString += `
+      UPDATE users
+      SET name = $2,
+      password = $3
+      WHERE id = $1`;
+    } else if (password === '') {
+      queryParams.push(userId);
+      queryParams.push(name);
+      queryParams.push(email);
+      queryString = `
+      UPDATE users
+      SET name = $2,
+      email = $3,
+      WHERE id = $1`;
+    } else {
+      queryParams.push(userId);
+      queryParams.push(name);
+      queryParams.push(email);
+      queryParams.push(hashedPassword);
+      queryString += `
+      UPDATE users
+      SET name = $2,
+      email = $3,
+      password = $4
+      WHERE id = $1`;
+    }
 
     db.query(queryString, queryParams)
       .then((data) => {
